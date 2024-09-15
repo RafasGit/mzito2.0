981e55f939924fe2daa7bb2d6b610005e5bcde48
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-09-15 16:14:23 +0300

    main branch

--- app/api/sendSms/route.ts
+++ app/api/sendSms/route.ts
@@ -1,36 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
-
-interface SmsRequest {
-  to: string;
-  from?: string;
-  message: string;
-}
-
-const API_KEY = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0MzciLCJvaWQiOjQzNywidWlkIjoiY2Y5YTVhMzQtZTY5Zi00ZWRhLWEzMDctODgxNGY3ZDk4Mjk1IiwiYXBpZCI6MzcyLCJpYXQiOjE3MjQ5MjM1NzUsImV4cCI6MjA2NDkyMzU3NX0.WvQhx_CxAAErh6VopJFuk59IHSDGTnIDUPFWAAMNkFcLCGSsjJX1aoLoZ23Ytw9IoQrJgVvmW1XGd5hw9mUtrw'
-const API_ENDPOINT = 'https://api2.tiaraconnect.io/api/messaging/sendsms'; // Replace with your actual API endpoint
-
-
+import { sendSmsServer } from '@/lib/useSmsSender';
 export async function POST(req: NextRequest) {
   try {
-    const { to, from = 'TIARACONECT', message }: SmsRequest = await req.json();
-console.log(from)
-    const response = await fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`,
-      },
-      body: JSON.stringify({ to, from, message }),
-    });
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      return NextResponse.json({ error: responseData }, { status: response.status });
-    }
-
-    return NextResponse.json(responseData, { status: 200 });
+    const { to, from = 'TIARACONECT', message } = await req.json();
+    return await sendSmsServer({ to, from, message });
   } catch (error) {
+    console.error('Error in SMS route:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }

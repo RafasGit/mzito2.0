bbafd7cee9cd3c0aa8580949aea521856af1fb81
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2024-09-16 08:44:35 +0300

    ux

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -388,7 +388,7 @@ const RenderInput = ({ field, props,  }: { field: any; props: CustomProps }) => 
     </SheetTrigger>
     <SheetContent className="sm:max-w-[425px]">
       <SheetHeader>
-         <SheetDescription className="mt-8 mb-4 text-xl sm:mt-10 sm:mb-6">Select Date and Time to book a service</SheetDescription> 
+         <SheetDescription className="mt-6 mb-4 text-xl sm:mt-10 sm:mb-6 sm:text-2xl">Select Date and Time to book a service</SheetDescription> 
       </SheetHeader>
       <FormControl>     
         <div className="grid gap-4 py-4">
